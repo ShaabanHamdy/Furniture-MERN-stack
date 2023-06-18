10ddd627cc8da625a6e181bdf1ba4fd955ad4e01
Shaaban <shabanhamdy94@gmail.com>
2023-06-18 22:53:11 +0300

    first commit

--- DB/connection.js
+++ DB/connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectionDB = async () => {
     // mongodb://localhost:27017
     return await mongoose
-        .connect(process.env.CONNECTION_ONLINE)
+        .connect("mongodb+srv://EcommerceFurniture:shabanhamdy@cluster0.e09gpvw.mongodb.net/EcommerceFurniture")
         .then(res => console.log("connection DB is Running"))
         .catch(err => console.log({ message: "fail in connection DB", err }))
 
